520da49dafd052fc790efe44469b5ea4dae1c8fe
carlos-vf <carlosvelazquezf@hotmail.com>
2025-01-14 10:08:46 +0100

    Fixed sidebar

--- src/web/web_service.py
+++ src/web/web_service.py
@@ -1,14 +1,21 @@
 import streamlit as st
 
 
+# Page configuration
+st.set_page_config(
+    page_title="LyrIAcs",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+
+# Page navigation
+pg = st.navigation([st.Page("input_lyrics.py"), st.Page("feature_selection.py")], position="hidden")
+
 # CSS
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
 load_css("styles.css")
 
 
-# Page configuration
-pg = st.navigation([st.Page("input_lyrics.py"), st.Page("feature_selection.py")])
 pg.run()
