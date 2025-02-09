ee2d1e7b68b999e03921d265c24574581cd6873b
vale <destasiovaleria16@gmail.com>
2025-02-09 22:32:49 +0100

    experimented to finally connected model with genre selection

--- src/web/feature_selection.py
+++ src/web/feature_selection.py
@@ -3,34 +3,29 @@ import streamlit as st
 import os
 # THIS LINE CHANGES BASED ON YOUR R VERSION/LOCATION
 os.environ["R_HOME"] = r"C:\Program Files\R\R-4.4.1"
+os.environ["R_LIBS_USER"]= r"C:/Users/desta/AppData/Local/R/win-library/4.4"
 from rpy2.robjects import r, pandas2ri
 from rpy2.robjects.packages import importr
 import rpy2.robjects.packages as rpackages
 import warnings
 import pandas as pd
 
-def install_r_package(package_name):
-    utils = rpackages.importr('utils')
-    utils.chooseCRANmirror(ind=1)  # Scegli uno specchio CRAN
-    utils.install_packages(package_name)
-
-# Installa i pacchetti uno alla volta
-install_r_package('tm')
-install_r_package('SnowballC')
-install_r_package('randomForest')
-install_r_package('caret')
-
 # Importa le librerie necessarie
-tm = importr('tm')
-SnowballC = importr('SnowballC')
-randomForest = importr('randomForest')
-caret = importr('caret')
+#tm = importr('tm')
+#SnowballC = importr('SnowballC')
+#randomForest = importr('randomForest')
+#caret = importr('caret')
 
 # Activate automatic conversion between R and pandas
-pandas2ri.activate()
+#pandas2ri.activate()
 
 # Load R model and functions
 r_code = """
+.libPaths("C:/Users/desta/AppData/Local/R/win-library/4.4")
+library(tm)
+library(SnowballC)
+library(randomForest)
+library(caret)
 model = readRDS("../../artifacts/rf_model.rds")
 dtm = readRDS("../../data/processed/DTM.rds")
 
@@ -44,9 +39,13 @@ genre_predict = function(text){
     text_corpus <- tm_map(text_corpus, stripWhitespace)
 
     new_dtm <- DocumentTermMatrix(text_corpus)
+    print(dim(new_dtm)) 
 
     new_terms <- colnames(new_dtm)
     old_terms <- colnames(dtm)
+
+    common_terms <- intersect(new_terms,old_terms)
+    print(common_terms)
 
     filtered_dtm <- matrix(0, nrow = nrow(new_dtm), ncol = length(old_terms))
     colnames(filtered_dtm) <- old_terms
@@ -55,12 +54,14 @@ genre_predict = function(text){
     filtered_dtm[, common_terms] <- as.matrix(new_dtm[, common_terms])
 
     predictions <- predict(model, filtered_dtm, type = "prob")
+    predictions <- as.matrix(predictions)
+    print(predictions)
     # Sort predictions by probability and get the top 3
-    sorted_indices <- order(predictions, decreasing = TRUE)
-    top_3_genres <- names(predictions)[sorted_indices[1:3]]
-    top_3_probabilities <- predictions[sorted_indices[1:3]]
+    sorted_indices <- order(predictions[1,], decreasing = TRUE)[1:3]
+    top_3_genres <- colnames(predictions)[sorted_indices]
     
-    return(list(top_3_genres, top_3_probabilities))
+    print(top_3_genres)
+    return(top_3_genres)
 }
 """
 r(r_code)  # Executes the R code
@@ -82,12 +83,11 @@ with st.spinner('Predicting genre...'):
     result = genre_predict(user_lyrics)  # Call R function
     st.success('Prediction complete!')
 
-
 with st.container(border=True):
     left, right = st.columns(2, vertical_alignment="center")
-    options = list(result[0])
-    emotions = left.pills("Choose a **emotion**:", options, selection_mode="multi")
-    emotions_random = right.pills("", ["Random "], selection_mode="single")
+    options = list(result)
+    genres = left.pills("Choose a **genre**:", options, selection_mode="multi")
+    genres_random = right.pills("", ["Random "], selection_mode="single")
 
 # Emotions Selection (single selection only)
 with st.container(border=True):
