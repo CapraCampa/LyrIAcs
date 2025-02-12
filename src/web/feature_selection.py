1459d5aba213182b41df682f7d0eb8c90bd1d722
vale <destasiovaleria16@gmail.com>
2025-02-12 10:01:42 +0100

    simple version that works!

--- src/web/feature_selection.py
+++ src/web/feature_selection.py
@@ -2,50 +2,7 @@ import streamlit as st
 
 import pickle
 import pandas as pd
-# from azure.storage.blob import BlobServiceClient
 
-# connection_string = st.secrets.get("AZURE_CONNECTION_STRING")
-# blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-# # Specifica il nome del contenitore e del blob
-# container_client = blob_service_client.get_container_client("ml-models")
-# blob_client = container_client.get_blob_client("model_genre.pkl")
-
-# # Scarica il file del modello
-# with open("model_genre_azure.pkl", "wb") as f:
-#     f.write(blob_client.download_blob().readall())
-
-# with open("model_genre_azure.pkl", "rb") as f:
-#     model_genre = pickle.load(f)
-
-# #model_genre = joblib.load('artifacts/model_compressed.pkl')
-# #with gzip.open('artifacts/model_genre_compressed.pkl.gz', 'rb') as f:
-# #    model_genre = joblib.load(f)
-# #with open('artifacts/model_genre.pkl', 'rb') as file:
-# #    model_genre = pickle.load(file)
-# with open('artifacts/vectorizer_genre.pkl', 'rb') as file:
-#     tfidf_vectorizer = pickle.load(file)
-# with open('artifacts/svd_genre.pkl', 'rb') as file:
-#     svd = pickle.load(file)
-# # Function definition
-# def predict_genre(text):
-#     # Ensure text is a valid string
-#     text = str(text) if not isinstance(text, str) else text
-
-#     if not text.strip():
-#         print("Input text is empty or invalid!")
-#         return ["Invalid or empty input"]
-#     # Predict the genre probabilities and get the top 3 predictions
-#     predicted_genre = model_genre.predict_proba(svd.transform(tfidf_vectorizer.transform([text])))
-#     top_three_indices = predicted_genre[0].argsort()[::-1][:3]
-#     predicted_genre = model_genre.classes_[top_three_indices]
-#     return predicted_genre
-    
-
-
-st.write(f"Input: {st.session_state.first_chunks}")
-st.write(f"Emotions: {st.session_state.emotions}")
-st.write(f"Genres: {st.session_state.genres}")
 
 
 # Title
@@ -60,25 +17,22 @@ if 'user_lyrics' in st.session_state:
 else:
     st.warning("No lyrics found. Please go back and input lyrics.")
 
-
-# with st.spinner('Predicting genre...'):
-#     result = predict_genre(user_lyrics)
-#     st.success('Prediction complete!')
+st.write(f"Input: {st.session_state.current_chunks}")
 
 # Genre Selection (single selection only)
 with st.container(border=True):
     left, right = st.columns(2, vertical_alignment="center")
     options = st.session_state.genres
-    genres = left.pills("Choose a **genre**:", options, selection_mode="multi")
-    genres_random = right.pills("", ["Random "], selection_mode="single")
+    genres = left.pills("Choose a **genre**:", options, selection_mode="multi",  key="genre_selection")
+    genres_random = right.pills("", ["Random "], selection_mode="single",  key="genre_random")
 
 
 # Emotions Selection (single selection only)
 with st.container(border=True):
     left, right = st.columns(2, vertical_alignment="center")
     options = st.session_state.emotions
-    emotions = left.pills("Choose a **emotion**:", options, selection_mode="multi")
-    emotions_random = right.pills("", ["Random "], selection_mode="single")
+    emotions = left.pills("Choose a **emotion**:", options, selection_mode="multi",  key="emotion_selection")
+    emotions_random = right.pills("", ["Random "], selection_mode="single",  key="emotion_random")
 
 cols = st.columns(6, vertical_alignment="center")
 if cols[-1].button("Continue"):
